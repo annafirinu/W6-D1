6ba7013e2b0bbf476fa0203131f3d46cbabf67eb
annafirinu <anna.firinu2@outlook.it>
2025-03-17 18:33:27 +0100

    ultima versione

--- form.js
+++ form.js
@@ -3,6 +3,11 @@ const ownerNameInput = document.getElementById("ownerName");
 const speciesInput = document.getElementById("species");
 const breedInput = document.getElementById("breed");
 
+let petList = document.getElementById("petList");
+let addButton = document.getElementById("add");
+
+let pets = [];
+
 class Pet {
   constructor(_petName, _ownerName, _species, _breed) {
     this.petName = _petName;
@@ -10,6 +15,7 @@ class Pet {
     this.species = _species;
     this.breed = _breed;
   }
+
   compareOwnerName = function (ownerCompare) {
     if (this.ownerName === ownerCompare.ownerName) {
       return true;
@@ -19,19 +25,34 @@ class Pet {
   };
 }
 
-let pets = [];
+const renderList = function () {
+  petList.innerHTML = ""; // <-- svuota la lista all'inizio
+  pets.forEach((pet) => {
+    const newLi = document.createElement("li");
+    newLi.innerText =
+      "Nome: " + pet.petName + ", proprietario: " + pet.ownerName;
+    petList.appendChild(newLi);
+  });
+};
 
 const form = document.getElementById("pet-form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const pet = new Pet(
+  const newPet = new Pet(
     petNameInput.value,
     ownerNameInput.value,
     speciesInput.value,
     breedInput.value
   );
-  console.log("Dati Animale", pet);
-  pets.push(pet);
+  console.log("Dati Animale", newPet);
+  pets.push(newPet);
   console.log(pets);
+  renderList();
+  petNameInput.value = ""; // <-- svuota l'input field alla fine
+  ownerNameInput.value = ""; // <-- svuota l'input field alla fine
+  speciesInput.value = ""; // <-- svuota l'input field alla fine
+  breedInput.value = ""; // <-- svuota l'input field alla fine
   form.reset();
 });
+
+console.log(petList.compareOwnerName(petList));
